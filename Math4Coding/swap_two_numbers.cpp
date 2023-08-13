b4ca520b48e809269eb6cb9ec5c00d09755c291d
Aqib <5294aqib@gmail.com>
2023-08-13 11:43:46 +0530

    Added Swap two numbers

---
+++ Math4Coding/swap_two_numbers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    public:
+    pair<int, int> swapNumbers(int a, int b) {
+        pair<int, int> myPair;
+        myPair.second=a;
+        myPair.first=b;
+        return myPair; 
+    }
+}
