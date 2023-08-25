87229ca0065bbb0cdff0fdc07c1590baad923bbd
Aqib <5294aqib@gmail.com>
2023-08-25 10:13:59 +0530

    Minor spaced added.

--- Arrays/maximum_subarray.cpp
+++ Arrays/maximum_subarray.cpp
@@ -5,7 +5,7 @@ public:
         int maxSoFar=nums[0];
         int maxEndinghere=nums[0];
         for(int i=1;i<nums.size();i++){
-            maxEndinghere=max(nums[i],maxEndinghere+nums[i]);
+            maxEndinghere = max(nums[i],maxEndinghere+nums[i]);
             maxSoFar = max(maxEndinghere,maxSoFar);
         }
         return maxSoFar;
