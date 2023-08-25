7037184a3695c616510de15563311ae78f88e303
aqib.ahmed <aqib.ahmed@samarth.ac.in>
2023-08-25 10:11:44 +0530

    Added Maximum subarray sum

---
+++ Arrays/maximum_subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // Your code here
+        int maxSoFar=nums[0];
+        int maxEndinghere=nums[0];
+        for(int i=1;i<nums.size();i++){
+            maxEndinghere=max(nums[i],maxEndinghere+nums[i]);
+            maxSoFar = max(maxEndinghere,maxSoFar);
+        }
+        return maxSoFar;
+    }
+};
